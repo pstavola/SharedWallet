7c9aae0c46c51eff4af2de4692e16692428bbb27
Patrizio Stavola <patrizio.stavola@gmail.com>
2023-07-18 16:47:14 +0200

    adding comments

--- src/SharedWallet.sol
+++ src/SharedWallet.sol
@@ -17,16 +17,29 @@ contract SharedWallet is
         uint timeLimit;
     }
 
+    /* ========== GLOBAL VARIABLES ========== */
+
+    ///@notice mapping to handle authorised addresses
+    mapping(address => User) public users;
+
+    /* ========== EVENTS ========== */
+
     event AllowanceRenewed(address indexed user, uint amount, uint timeLimit);
     event CoinsSent(address indexed receiver, uint amount);
     event Deposit(uint amount);
 
-    mapping(address => User) public users;
+    /* ========== FUNCTIONS ========== */
 
     receive() external payable onlyOwner {
         emit Deposit(msg.value);
     }
 
+    /**
+     * @notice renewAllowance renew/create a new allowance. Only owner of the contract can add allowances. It checks if allowance amount is more than contract balance and stores user allowance in gloabal variable
+     * @param _user user address for the new allowance
+     * @param _amount amount of coins to be allowed
+     * @param _timeLimit validity period in seconds
+    */
     function renewAllowance(address _user, uint _amount, uint _timeLimit) public onlyOwner {
         require(_amount <= address(this).balance, "Allowance exceeds contract current balance");
 
@@ -38,6 +51,11 @@ contract SharedWallet is
         emit AllowanceRenewed(_user, _amount, _timeLimit);
     }
 
+    /**
+     * @notice send coins to the desired address. It checks that contract has enough coins to send and that user allowance is repsected for amount and validity period
+     * @param _amount amount of coins to be sent
+     * @param _receiver address to which coins will be sent
+    */
     function sendCoins(uint _amount, address payable _receiver) public {
         User memory userAllowance = users[msg.sender];
         require(_amount > 0, "Amount must be more than 0");
@@ -54,10 +72,18 @@ contract SharedWallet is
         emit CoinsSent(_receiver, _amount);
     }
 
+    /**
+     * @notice send coins to the desired address. It checks that contract has enough coins to send and that user allowance is repsected for amount and validity period
+     * @return uint amount defined in user allowance
+    */
     function checkAllowance() public view returns (uint) {
         return(users[msg.sender].amount);
     }
 
+    /**
+     * @notice send coins to the desired address. It checks that contract has enough coins to send and that user allowance is repsected for amount and validity period
+     * @return uint amount of contract balance
+    */
     function checkBalance() public view returns (uint) {
         return(address(this).balance);
     }
